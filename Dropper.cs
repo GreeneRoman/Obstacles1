533ef755b383d19ae2ee56856a84e6cf846225d8
GreeneRoman <43356950+GreeneRoman@users.noreply.github.com>
2022-10-21 11:37:01 +0200

    Add files via upload

---
+++ Dropper.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
+  [SerializeField]  float timeToWait=5f;
+  
+    void Start()
+    {
+       renderer= GetComponent<MeshRenderer>();
+       rigidbody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
+    }
+
+    
+    void Update()
+    {
+       // Debug.Log(Time.time);
+       if (Time.time> timeToWait ){
+        renderer.enabled = true;
+        rigidbody.useGravity = true;
+        //Debug.Log("3 seconds has elapsed");
+       }
+    }
+}
